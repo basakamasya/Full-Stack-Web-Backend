f2236beb03587ea7f7d3522ca1dd0b5e512f17ff
unknown <basak.amasya@aalto.fi>
2022-12-09 17:10:18 +0200

    3.5 implemented

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http')
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   { 
     "id": 1,
@@ -26,12 +28,11 @@ let persons = [
   }
 ]
 
-/*
-{ 
-  "id": 4,
-  "name": "Mary Poppendieck", 
-  "number": "39-23-6423122"
-}*/
+//taken from the example
+const generateId = () => {
+  const randId = Math.ceil(Math.random() * 100000); //big enough random id
+  return randId;
+}
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
@@ -60,6 +61,25 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
